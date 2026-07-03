7c1aa20965f1ad2b7a5e1f0b9fce31b703563101
Valentina Ferro <valentina-ferro@hotmail.it>
2026-07-03 16:09:50 +0200

    fetch

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,34 +1,19 @@
 import { Component } from "react";
 
-const movies = [
-  {
-    Title: "Harry Potter and the Deathly Hallows: Part 2",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BOTA1Mzc2N2ItZWRiNS00MjQzLTlmZDQtMjU0NmY1YWRkMGQ4XkEyXkFqcGc@._V1_SX300.jpg",
-  },
-  {
-    Title: "Harry Potter and the Sorcerer's Stone",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNTU1MzgyMDMtMzBlZS00YzczLThmYWEtMjU3YmFlOWEyMjE1XkEyXkFqcGc@._V1_SX300.jpg",
-  },
-  {
-    Title: "Harry Potter and the Prisoner of Azkaban",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMTY4NTIwODg0N15BMl5BanBnXkFtZTcwOTc0MjEzMw@@._V1_SX300.jpg",
-  },
-  {
-    Title: "Harry Potter and the Chamber of Secrets",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNGJhM2M2MWYtZjIzMC00MDZmLThkY2EtOWViMDhhYjRhMzk4XkEyXkFqcGc@._V1_SX300.jpg",
-  },
-];
-
 class Cards extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movies: movies,
+      movies: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://www.omdbapi.com/?apikey=24ad60e9&s=jurassic%20park")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ movies: data.Search });
+      });
   }
 
   render() {
