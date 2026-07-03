a4c1aa3894ba27c7f581541ab6858690eb8638e0
Valentina Ferro <valentina-ferro@hotmail.it>
2026-07-03 16:45:50 +0200

    footer

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,37 +1,73 @@
 import { Component } from "react";
 
+const OMDB_API_KEY = "24ad60e9";
+
 class Cards extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movies: [],
+      categories: [
+        { title: "Jurassic Park", query: "jurassic park", movies: [] },
+        { title: "Harry Potter", query: "harry potter", movies: [] },
+        { title: "Minions", query: "minions", movies: [] },
+      ],
+      error: null,
     };
   }
 
   componentDidMount() {
-    fetch("https://www.omdbapi.com/?apikey=24ad60e9&s=jurassic%20park")
-      .then((res) => res.json())
-      .then((data) => {
-        this.setState({ movies: data.Search });
-      });
+    this.state.categories.forEach((category, catIndex) => {
+      fetch(
+        `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${encodeURIComponent(
+          category.query
+        )}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          this.setState((prevState) => {
+            const categories = [...prevState.categories];
+            categories[catIndex] = {
+              ...categories[catIndex],
+              movies: data.Search || [],
+            };
+            return { categories };
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+          this.setState({ error: "Unable to load the movies" });
+        });
+    });
   }
 
   render() {
-    const movieCards = this.state.movies.map((movie, index) => {
-      return (
-        <div key={index}>
-          <img
-            src={movie.Poster}
-            alt={movie.Title}
-            onError={(e) =>
-              (e.target.src = "https://placehold.co/300x445?text=No+Poster")
-            }
-          />
-        </div>
-      );
-    });
+    if (this.state.error) {
+      return <p className="cards-error">{this.state.error}</p>;
+    }
 
-    return <>{movieCards}</>;
+    return (
+      <div className="rows-container">
+        {this.state.categories.map((category, catIndex) => (
+          <div className="movie-row" key={catIndex}>
+            <h2 className="row-title">{category.title}</h2>
+            <div className="row-scroll">
+              {category.movies.map((movie, index) => (
+                <div className="movie-card" key={index}>
+                  <img
+                    src={movie.Poster}
+                    alt={movie.Title}
+                    onError={(e) =>
+                      (e.target.src =
+                        "https://placehold.co/300x445?text=No+Poster")
+                    }
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,45 @@
 import { Component } from "react";
 
+const columns = [
+  ["Audio and Subtitles", "Media Center", "Privacy", "Contact us"],
+  ["Audio Description", "Investor Relations", "Legal Notices"],
+  ["Help Center", "Jobs", "Cookie Preferences"],
+  ["Gift Cards", "Terms of Use", "Corporate Information"],
+];
+
+const socials = ["Facebook", "Instagram", "Twitter", "YouTube"];
+
 class Footer extends Component {
   render() {
-    return <footer></footer>;
+    return (
+      <footer className="app-footer">
+        <div className="footer-socials">
+          {socials.map((social, index) => (
+            <a href="#" key={index} aria-label={social}>
+              {social[0]}
+            </a>
+          ))}
+        </div>
+
+        <div className="footer-columns">
+          {columns.map((column, colIndex) => (
+            <ul className="footer-column" key={colIndex}>
+              {column.map((link, linkIndex) => (
+                <li key={linkIndex}>
+                  <a href="#">{link}</a>
+                </li>
+              ))}
+            </ul>
+          ))}
+        </div>
+
+        <button type="button" className="footer-service-code">
+          Service Code
+        </button>
+
+        <p className="footer-copy">&copy; 1997-2026 Netflix, Inc.</p>
+      </footer>
+    );
   }
 }
 
